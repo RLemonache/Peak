28843c3a636bdebfb228971d13c5e24cc076f76d
RLemonache <49251607+RLemonache@users.noreply.github.com>
2022-03-30 21:46:29 -0300

    Add project files.

---
+++ Controllers/CadastrarController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Peak.Controllers
+{
+    [ApiController]
+    [Route("Cadastrar")]
+    public class CadastroCalculo : Controller
+    {
+        public Decimal Calculo(int parcelas, decimal valor)
+        {
+            var resultado = valor * parcelas;
+            var juros = Decimal.Multiply(resultado, (decimal)0.05);
+            var total = resultado + juros;
+
+            return total;
+        }
+    }
+}
---
+++ Controllers/ConsultaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Peak.Controllers
+{
+    [ApiController]
+    [Route("Consultar")]
+    public class ConsultaController : Controller
+    {
+        [HttpGet]
+        public String Consulta(int indice)
+        {
+            var lista = new List<KeyValuePair<int, string>>();
+            lista.Add(new KeyValuePair<int, string>(1, "João"));
+            lista.Add(new KeyValuePair<int, string>(2, "Maria"));
+            lista.Add(new KeyValuePair<int, string>(3, "Ana"));
+
+            var nome = lista[indice-1].Value;
+
+            return nome;
+        }
+    }
+}
